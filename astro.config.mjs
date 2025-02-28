1fd192753729ee8d1d7dfa49e756ca2f1b736804
kypes <jay@kypers.ca>
2025-02-28 07:40:50 -0500

    Added React and TailwindCSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,17 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import cloudflare from "@astrojs/cloudflare";
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
